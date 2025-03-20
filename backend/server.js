56617ac6f5f38b28973d0a8733510f16fbad25ba
StefanJ <jitianu_stefan@yahoo.com>
2025-03-20 15:34:18 +0200

    serving & fetching data

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotnev from 'dotenv';
+dotnev.config();
+import products from './data/products.js';
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('API is running...J');
+});
+
+app.get('/api/products', (req, res) => {
+    res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id);
+    res.json(product);
+})
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import products from '../products';
+import axios from 'axios';
+
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() =>{
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []); //array of dependencies empty array inseamna ca ruleaza doar 1 data atunci cand se incarca pagina 
   return (
     <>
         <h1>Latest Products</h1>
