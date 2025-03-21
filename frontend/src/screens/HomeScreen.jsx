0a00858dce9865ee723ee409d28717eb3d19383d
StefanJ <jitianu_stefan@yahoo.com>
2025-03-21 14:00:16 +0200

    reedux toolkit setup and state management

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,23 +1,35 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import axios from 'axios';
+import { useGetProductsQuery } from '../slices/productsApiSlice';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  //const [products, setProducts] = useState([]);
 
-  useEffect(() =>{
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
-      setProducts(data);
-    };
+  // useEffect(() =>{
+  //   const fetchProducts = async () => {
+  //     const { data } = await axios.get('/api/products');
+  //     setProducts(data);
+  //   };
 
-    fetchProducts();
-  }, []); //array of dependencies empty array inseamna ca ruleaza doar 1 data atunci cand se incarca pagina 
+  //   fetchProducts();
+  // }, []); //array of dependencies empty array inseamna ca ruleaza doar 1 data atunci cand se incarca pagina 
+  const { data: products, isLoading, error } = useGetProductsQuery();
+
+  
   return (
     <>
+      { isLoading ? 
+      (<Loader/>) 
+      : error ? 
+      (
+      <Message variant='danger'>{error?.data?.message || error.error}</Message>
+      ) : 
+      (
+        <>
         <h1>Latest Products</h1>
         <Row>
             { products.map((product) => (
@@ -26,6 +38,8 @@ const HomeScreen = () => {
                 </Col>
             )) }
         </Row>
+        </>
+      )}
     </>
   )
 }
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,27 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useParams} from "react-router-dom";
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import axios from 'axios';
+import { useGetProductsDetailsQuery } from '../slices/productsApiSlice';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
 const ProductScreen = () => {
-    const [product, setProduct] = useState({});
-
     const { id: productId } = useParams();
+    const { data: product, isLoading, error} = useGetProductsDetailsQuery(productId);
+    //const [product, setProduct] = useState({});
+    // useEffect(() => {
+    //     const fetchProduct = async () => {
+    //         const { data } = await axios.get(`/api/products/${productId}`);
+    //         setProduct(data);
+    //     }
 
-    useEffect(() => {
-        const fetchProduct = async () => {
-            const { data } = await axios.get(`/api/products/${productId}`);
-            setProduct(data);
-        }
-
-        fetchProduct();
-    }, [productId]);
+    //     fetchProduct();
+    // }, [productId]);
 
   return <>
     <Link className="btn btn-light my-3" to="/">Inapoi</Link>
-    <Row>
+    { isLoading ? 
+    ( <Loader /> ) 
+    : error ? 
+    (
+      <Message variant='danger'>{error?.data?.message || error.error}</Message>
+    ) : 
+    (
+        <Row>
         <Col md={5}>
             <Image src={product.image} alt={product.name} fluid />
         </Col>
@@ -72,6 +80,7 @@ const ProductScreen = () => {
             </Card>
         </Col>
     </Row>
+    )}
 </>;
 }
 
