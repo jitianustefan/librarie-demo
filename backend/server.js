3425f723c5b4c6e2bbe6134c5f4a250f7ee6853e
StefanJ <jitianu_stefan@yahoo.com>
2025-03-21 12:38:29 +0200

    modeling out data, seeding sample data, custom error middleware, product controller

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import dotnev from 'dotenv';
 dotnev.config();
-import products from './data/products.js';
+import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import productRoutes from './routes/productRoutes.js';
 const port = process.env.PORT || 5000;
+
+connectDB(); // Connect to MongoDB
 
 const app = express();
 
@@ -10,13 +14,9 @@ app.get('/', (req, res) => {
     res.send('API is running...J');
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-})
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
