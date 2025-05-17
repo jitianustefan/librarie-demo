938b684ddb0e763728a4063d95e351c5a1a4e088
StefanJ <jitianu_stefan@yahoo.com>
2025-05-17 16:25:57 +0300

    typo

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 if (process.env.NODE_ENV === 'production') {
     // set statis folder
-    app.use(express.static(path.join(__dirname, '/forntend/build')));
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
 
     //any route that is not api will be replaced to index.html
     app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend','build', 'index.html')));
